5c5c3780605c8ba29103d0f94f514a49263d719f
AaryanPalve5 <aspalve2004@gmail.com>
2024-11-24 17:49:51 +0530

    config updated

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true, // Enable React Strict Mode
+    distDir: 'build', // Customize build directory
+  };
+  
+  export default nextConfig;
+  
